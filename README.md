226bf5b08832e7e6df6aa748ee553003cf37f3a1
Zacharie Montreuil <zmontreuil@rrc.ca>
2024-01-16 12:42:51 -0600

    Initial Commit: Added module and first python documentation

---
+++ README.md
@@ -0,0 +1,7 @@
+# Module 02 Demonstration
+
+## Description
+Introduction to Python: Variables, Documentation, and Printing
+
+## Author
+Zacharie Montreuil
---
+++ module_02.py
@@ -0,0 +1,6 @@
+"""
+Description: Module 02 Demonstration
+Author: Zacharie Montreuil
+Date: 16 January 2024
+Usage: Demonstrate content from Module 02
+"""
